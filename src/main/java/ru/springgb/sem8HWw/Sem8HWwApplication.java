24b7837f5599a9991afab3f4164fe479d40b6cfb
danil <danila_monster@mail.ru>
2024-02-12 13:28:29 +0300

    commit2

---
+++ src/main/java/ru/springgb/sem8HWw/Sem8HWwApplication.java
@@ -0,0 +1,13 @@
+package ru.springgb.sem8HWw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem8HWwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sem8HWwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/springgb/sem8HWw/Sem8HWwApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.springgb.sem8HWw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem8HWwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
